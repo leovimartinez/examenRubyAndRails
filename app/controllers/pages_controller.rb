fc7bb182750636e75c59b599e7e19b3c9470cf08
leovimartinez <leovioz@hotmail.com>
2016-08-04 22:01:03 -0500

    Se agregan los formularios y se persisten los votos

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,4 +7,9 @@ class PagesController < ApplicationController
 
   def batmanVsSuperman
   end
+
+  def votar
+  	Voto.create(heroe: params[:heroe], email: params[:mail])
+  	redirect_to pages_batmanVsSuperman_path, notice: "Su voto ha sido contabilizado"
+  end
 end
